1ff256d96db9f7c5fe3879b328a23f2db8dc6826
Fedhori <edhori990611@gmail.com>
2021-01-22 17:46:29 +0900

    Solve HomeRepository.kt bug

--- app/src/main/java/com/example/cashassignment/repository/HomeRepository.kt
+++ app/src/main/java/com/example/cashassignment/repository/HomeRepository.kt
@@ -39,21 +39,7 @@ class HomeRepository(application: Application) {
 
         notLoginBannerApi.getBanners(country).enqueue(object : Callback<List<BannerEntity>> {
             override fun onResponse(call: Call<List<BannerEntity>>, response: Response<List<BannerEntity>>) {
-
-                val bannerEntities = response.body()?.toList()
-
-                if (bannerEntities != null) {
-                    for(bannerEntity in bannerEntities){
-                        Log.d("test", " \nname: ${bannerEntity.name} \n" +
-                                "type: ${bannerEntity.type} \n" +
-                                "Url: ${bannerEntity.thumbnailUrl} \n" +
-                                "appGuideCoreId: ${bannerEntity.appGuideCoreId} \n" +
-                                "taskId: ${bannerEntity.taskId} \n" +
-                                "idx: ${bannerEntity.idx} \n\n")
-                    }
-                }
-
-                liveData.value = bannerEntities
+                liveData.value = response.body()?.toList()
             }
 
             override fun onFailure(call: Call<List<BannerEntity>>, t: Throwable) {
@@ -72,18 +58,11 @@ class HomeRepository(application: Application) {
 
     fun getNotLoginTaskData(country: String, page: Int = 0, category: TaskCategory = TaskCategory.ALL, orderStrategy: TaskOrderStrategy = TaskOrderStrategy.NEW): LiveData<List<TaskEntity>>{
 
-        val TaskEntities = MutableLiveData<List<TaskEntity>>()
+        val liveData = MutableLiveData<List<TaskEntity>>()
 
         notLoginTaskApi.getTasks(country, page, category, orderStrategy).enqueue(object : Callback<List<TaskEntity>> {
             override fun onResponse(call: Call<List<TaskEntity>>, response: Response<List<TaskEntity>>) {
-
-                val TaskEntities = response.body()?.toList()
-
-                if (TaskEntities != null) {
-                    for(TaskEntity in TaskEntities){
-                        Log.d("Result", " \nname: ${TaskEntity.title}")
-                    }
-                }
+                liveData.value = response.body()?.toList()
             }
 
             override fun onFailure(call: Call<List<TaskEntity>>, t: Throwable) {
@@ -91,6 +70,6 @@ class HomeRepository(application: Application) {
             }
         })
 
-        return TaskEntities
+        return liveData
     }
 }
