27798881bfbc5e9c3ff92b51fc13371d93f452b1
Fedhori <edhori990611@gmail.com>
2021-03-08 17:58:41 +0900

    dasdsadsa

--- app/src/main/java/com/example/cashassignment/repository/HomeRepository.kt
+++ app/src/main/java/com/example/cashassignment/repository/HomeRepository.kt
@@ -28,6 +28,7 @@ import kotlinx.coroutines.launch
 class HomeRepository : CoroutineScope by MainScope(){
 
     private val sadasff = 1
+    private val adasf = 1
 
     private val baseService = BaseService.getInstance()
 
